2ed89ed2bd8cac921449d4e3eb7786740803d7ae
TylerOrcutt <tyorcutt@gmail.com>
2019-05-05 20:02:53 -0400

    ops

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 
-ln -s ~/git/arch-dotfiles/.vimrc/ ~/.vimrc
+ln -s ~/git/arch-dotfiles/.vimrc ~/.vimrc
 ln -s ~/git/arch-dotfiles/.vim ~/.vim
-
 ln -s ~/git/arch-dotfiles/.config/nvim ~/.config/nvim
