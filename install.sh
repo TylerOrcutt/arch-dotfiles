a81838636852356b29852abfa83c820e9dd6402b
TylerOrcutt <tyorcutt@gmail.com>
2021-06-09 20:40:00 -0400

    removed old garbage

--- install.sh
+++ install.sh
@@ -1,13 +1,2 @@
 #! /bin/bash
 
-#vim
-ln -s ~/git/arch-dotfiles/.config/nvim/init.vim ~/.vimrc
-ln -s ~/git/arch-dotfiles/.config/nvim ~/.config/nvim
-
-#i3
-ln -s ~/git/arch-dotfiles/.config/i3blocks ~/.config/i3blocks
-ln -s ~/git/arch-dotfiles/.config/i3 ~/.config/i3
-
-#scripts
-ln -s ~/git/arch-dotfiles/.scripts/statusbar/ ~/.scripts/statusbar
-ln -s ~/git/arch-dotfiles/.scripts/i3cmds/ ~/.scripts/i3cmds
